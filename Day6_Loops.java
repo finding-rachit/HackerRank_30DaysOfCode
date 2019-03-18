80e6f69ea8aa67a69462b5d41512b0d7873e02d9
Rachit Sharma <36709071+finding-rachit@users.noreply.github.com>
2019-03-18 09:02:53 -0400

    Add files via upload

---
+++ Day6_Loops.java
@@ -0,0 +1,14 @@
+
+public class Day6_Loops {
+
+
+    public static void main(String[] args) {
+        int n = 2;
+
+        for (int i = 1; i < 11; i++)
+        {
+            int mult = n * i;
+            System.out.println(n +" x " + i + " = " + mult);
+        }
+    }
+}
